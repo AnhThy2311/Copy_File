7d34f3af5d8d2a6b479609136b7f5ea5dc32f1f3
HP <HP@DESKTOP-TK2OJN6>
2023-11-01 15:50:19 +0700

    commit 22

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import controller.CopyFile;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+    public static void main(String[] args) {
+        new CopyFile().run();
+    }
+}
